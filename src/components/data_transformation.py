4c248a4c7b8f84489dda3b74debc332ac2b74483
Parham <parham2013g@gmail.com>
2023-09-09 17:50:54 +0330

    Bug fixes for data transformation and data ingestion

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -36,14 +36,14 @@ class DataTransformation:
             num_pipeline = Pipeline(
                 steps=[
                     ('imputer', SimpleImputer(strategy='median')),
-                    ('scaler', StandardScaler()),
+                    ('scaler', StandardScaler(with_mean=False)),
                 ]
             )
             cat_pipeline = Pipeline(
                 steps=[
                     ('imputer', SimpleImputer(strategy='most_frequent')),
                     ('one_hot_encoder', OneHotEncoder()),
-                    ('scaler', StandardScaler())
+                    ('scaler', StandardScaler(with_mean=False))
                 ]
             )
             logging.info(f'Categorical columns: {categorical_columns}')
@@ -69,7 +69,6 @@ class DataTransformation:
             test_df = pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
-
             logging.info("Obtaining preprocessing object")
 
             preprocessing_obj = self.get_data_transformer_object()
@@ -87,12 +86,12 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
+            # Passing train and test data through preprocessing pipelines
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
-            ]
+            # Pay attention, we only transformed the feature, and not the target
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
@@ -111,3 +110,4 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e, sys)
+
